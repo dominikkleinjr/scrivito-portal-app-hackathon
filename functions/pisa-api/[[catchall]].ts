6541a47b994f19c4ac1e9aaf6420e6be44d07948
Alexander Pepper <alexander.pepper@justrelate.com>
2023-10-25 11:50:21 +0200

    Setup correct pisa proxy (slot 093)

--- functions/pisa-api/[[catchall]].ts
+++ functions/pisa-api/[[catchall]].ts
@@ -1,13 +1,6 @@
 export const onRequest: PagesFunction = async function onRequest({ request }) {
-  // TODO: 🛑🚧🛑 Remove the following Error,
-  // once folder name, `const pathPrefix` (both currently `/example-proxy') and `const target`
-  // are adjusted to your needs.
-  throw new Error(
-    'Incomplete example-proxy configuration! See `functions/example-proxy/[[catchall]].ts` for details.',
-  )
-
-  const pathPrefix = '/example-proxy'
-  const target = 'https://myservice.example.com/api'
+  const pathPrefix = '/pisa-api'
+  const target = 'https://web093.crm.pisasales.de/hackathon'
 
   const actualRequestUrl = request.url.replace(
     `${new URL(request.url).origin}${pathPrefix}`,
